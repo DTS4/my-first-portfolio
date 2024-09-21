5ab7c534e75f108b26471a0848948a97e43c24b5
Keith <keith.githinji@student.moringaschool.com>
2024-09-21 18:36:40 +0300

    Updated all files

--- script.js
+++ script.js
@@ -2,3 +2,25 @@ const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 
 document.querySelector("body").appendChild(h2);
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const navLinks = document.querySelectorAll("nav ul li a");
+
+    navLinks.forEach(function (link) {
+        link.addEventListener("click", function (event) {
+
+            event.preventDefault();
+
+            const targetSection = document.querySelector(this.getAttribute("href"));
+
+            targetSection.scrollIntoView({
+                behavior: "smooth",
+                block: "start"
+            });
+
+            navLinks.forEach((navLink) => navLink.classList.remove("active"));
+            this.classList.add("active");
+        });
+    });
+});
